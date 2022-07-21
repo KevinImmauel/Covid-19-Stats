7975f95f3586cfaed1c68fc180bb04f1700d111c
Kevin Immanuel <kevin.exe4real@gmail.com>
2022-07-21 23:02:23 +0530

    done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,36 @@
 <script>
-
-
 	let country = "";
-	var cases;
-	var casestodayindia;
-	var deathstoday;
-	var recovered;
-	var todayrecoveredindia;
-	var activeindia;
+
 	var totaldeaths;
 	var todayCases;
 	var active;
 	var todayRecovered;
 	var critical;
 
+	var casescountry;
+	var casestodaycountry;
+	var deathstodaycountry;
+	var todayrecoveredcountry;
+	var recoveredcountry;
+	var activecountry;
+	var deathscountry;
+	var criticalcountry;
+	var casesPerOneMillioncountry;
+	var deathsPerOneMillioncountry;
+	var testsPerOneMillioncountry;
+	var testscountry;
 
-
-	const dataall = 'https://disease.sh/v3/covid-19/all'
+	const dataall = "https://disease.sh/v3/covid-19/all";
 
 	fetch(dataall)
 		.then((response) => response.json())
 		.then((data) => {
-			totaldeaths = data["deaths"]
-			todayCases = data["todayCases"]
-			active = data["active"]
-			todayRecovered = data["todayRecovered"]
-			critical = data["critical"]
-		})
+			totaldeaths = data["deaths"];
+			todayCases = data["todayCases"];
+			active = data["active"];
+			todayRecovered = data["todayRecovered"];
+			critical = data["critical"];
+		});
 
 	function search() {
 		let datacountry = `https://disease.sh/v3/covid-19/countries/${country}`;
@@ -34,13 +38,18 @@
 		fetch(datacountry)
 			.then((response) => response.json())
 			.then((data) => {
-				cases = data["cases"];
-				casestodayindia
-		 = data["todayCases"];
-				deathstoday = data["todayDeaths"];
-				recovered = data["recovered"];
-				todayrecoveredindia = data["todayrecoveredindia"];
-				activeindia = data["activeindia"];
+				casescountry = data["cases"];
+				casestodaycountry = data["todayCases"];
+				deathstodaycountry = data["todayDeaths"];
+				recoveredcountry = data["recovered"];
+				todayrecoveredcountry = data["todayRecovered"];
+				activecountry = data["active"];
+				deathscountry = data["deaths"];
+				criticalcountry = data["critical"];
+				casesPerOneMillioncountry = data["casesPerOneMillion"];
+				deathsPerOneMillioncountry = data["deathsPerOneMillion"];
+				testsPerOneMillioncountry = data["testsPerOneMillion"];
+				testscountry = data["tests"];
 			});
 	}
 </script>
@@ -58,31 +67,31 @@
 		<div class="stats stats-vertical lg:stats-horizontal shadow">
 			<div class="stat h-40 w-68">
 				<div class="stat-title">New Cases Today</div>
-				<div class="stat-value text-6xl">{todayCases%1000}K</div>
+				<div class="stat-value text-6xl">{todayCases % 1000}K</div>
 				<div class="stat-desc">↗︎ 40 (10%)</div>
 			</div>
 
 			<div class="stat h-40 w-68">
 				<div class="stat-title">Active Cases</div>
-				<div class="stat-value text-6xl">{active%1000}K</div>
+				<div class="stat-value text-6xl">{active % 1000}K</div>
 				<div class="stat-desc">↗︎ 400 (22%)</div>
 			</div>
 
 			<div class="stat h-40 w-68">
 				<div class="stat-title">Total Deaths</div>
-				<div class="stat-value text-6xl">{totaldeaths%1000}K</div>
+				<div class="stat-value text-6xl">{totaldeaths % 1000}K</div>
 				<div class="stat-desc">↘︎ 90 (14%)</div>
 			</div>
 
 			<div class="stat h-40 w-68">
 				<div class="stat-title">Today Recovered</div>
-				<div class="stat-value text-6xl">{todayRecovered%1000}K</div>
+				<div class="stat-value text-6xl">{todayRecovered % 1000}K</div>
 				<div class="stat-desc">↘︎ 90 (14%)</div>
 			</div>
 
 			<div class="stat h-40 w-68">
 				<div class="stat-title">Critical</div>
-				<div class="stat-value text-6xl">{critical%1000}K</div>
+				<div class="stat-value text-6xl">{critical % 1000}K</div>
 				<div class="stat-desc">↘︎ 90 (14%)</div>
 			</div>
 		</div>
@@ -170,39 +179,63 @@
 
 	<div class="row-span-2 col-span-3 px-10 pt-4">
 		<div class="shadow-lg rounded-lg overflow-hidden">
-			<div class="py-3 px-5 bg-base-300">Line chart</div>
-			<canvas class="p-10" id="chartLine"></canvas>
-		  </div>
-		  
-		  <!-- Required chart.js -->
-		  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-		  
-		  <!-- Chart line -->
-		  <script>
-			const labels = ["January", "February", "March", "April"];
+			<div class="py-3 px-5 bg-base-300">
+				Number of Cases since last 30 days
+			</div>
+			<canvas class="p-10" id="chartLine" />
+		</div>
+
+		<!-- Required chart.js -->
+		<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+		<!-- Chart line -->
+		<script>
+			
+			var datahistory = `https://disease.sh/v3/covid-19/historical/india`;
+
+			var datalist = [];
+			var labellist = [];
+
+			fetch(datahistory)
+				.then((response) => response.json())
+				.then((data) => {
+					for (
+						let i = 0;
+						i < Object.keys(data["timeline"]["cases"]).length;
+						i++
+					) {
+						var j = Object.keys(data["timeline"]["cases"])[i];
+						datalist.push(data["timeline"]["cases"][j]);
+						labellist.push(
+							j.slice(2, -1).slice(0, 2).replace("/", "")
+						);
+					}
+				});
+
+			const labels = labellist;
 			const data = {
-			  labels: labels,
-			  datasets: [
-				{
-				  label: "My First dataset",
-				  backgroundColor: "hsl(252, 82.9%, 67.8%)",
-				  borderColor: "hsl(252, 82.9%, 67.8%)",
-				  data: [0, 10, 5, 2, 20],
-				},
-			  ],
+				labels: labels,
+				datasets: [
+					{
+						label: "Past 30 days",
+						backgroundColor: "hsl(252, 82.9%, 67.8%)",
+						borderColor: "hsl(252, 82.9%, 67.8%)",
+						data: datalist,
+					},
+				],
 			};
-		  
+
 			const configLineChart = {
-			  type: "line",
-			  data,
-			  options: {},
+				type: "line",
+				data,
+				options: {},
 			};
-		  
+
 			var chartLine = new Chart(
-			  document.getElementById("chartLine"),
-			  configLineChart
+				document.getElementById("chartLine"),
+				configLineChart
 			);
-		  </script>
+		</script>
 	</div>
 
 	<div class="p-4 col-span-2">
@@ -222,53 +255,53 @@
 					<tr>
 						<th class="bg-base-300">1</th>
 						<td class="bg-base-300">Total Cases</td>
-						<td class="bg-base-300">{cases}</td>
-						<td class="bg-base-300 font-bold">4</td>
-						<td class="bg-base-300">activeindia Cases</td>
-						<td class="bg-base-300">{activeindia}</td>
+						<td class="bg-base-300">{casescountry}</td>
+						<td class="bg-base-300 font-bold">7</td>
+						<td class="bg-base-300">Active Cases</td>
+						<td class="bg-base-300">{activecountry} </td>
 					</tr>
 					<!-- row 2 -->
 					<tr>
 						<th class="bg-base-300">2</th>
 						<td class="bg-base-300">Total Recovered</td>
-						<td class="bg-base-300">{recovered}</td>
-						<td class="bg-base-300 font-bold">5</td>
+						<td class="bg-base-300">{recoveredcountry}</td>
+						<td class="bg-base-300 font-bold">8</td>
 						<td class="bg-base-300">Cases Today</td>
-						<td class="bg-base-300">{casestodayindia
-				}</td>
+						<td class="bg-base-300">{todayrecoveredcountry} </td>
 					</tr>
 					<!-- row 3 -->
 					<tr>
 						<th class="bg-base-300">3</th>
 						<td class="bg-base-300">Total Deaths</td>
-						<td class="bg-base-300">34749</td>
-						<td class="bg-base-300 font-bold">6</td>
+						<td class="bg-base-300">{deathscountry}</td>
+						<td class="bg-base-300 font-bold">9</td>
 						<td class="bg-base-300">Deaths Today</td>
-						<td class="bg-base-300">{deathstoday}</td>
+						<td class="bg-base-300">{deathstodaycountry}</td>
 					</tr>
 					<tr>
-						<th class="bg-base-300">3</th>
-						<td class="bg-base-300">Total Deaths</td>
-						<td class="bg-base-300">34749</td>
-						<td class="bg-base-300 font-bold">6</td>
-						<td class="bg-base-300">Deaths Today</td>
-						<td class="bg-base-300">{deathstoday}</td>
+						<th class="bg-base-300">4</th>
+						<td class="bg-base-300">Cases Per 1 Million</td>
+						<td class="bg-base-300">{casesPerOneMillioncountry}</td>
+						<td class="bg-base-300 font-bold">10</td>
+						<td class="bg-base-300">New Cases Today</td>
+						<td class="bg-base-300">{casestodaycountry}</td>
 					</tr>
 					<tr>
-						<th class="bg-base-300">3</th>
-						<td class="bg-base-300">Total Deaths</td>
-						<td class="bg-base-300">34749</td>
-						<td class="bg-base-300 font-bold">6</td>
-						<td class="bg-base-300">Deaths Today</td>
-						<td class="bg-base-300">{deathstoday}</td>
+						<th class="bg-base-300">5</th>
+						<td class="bg-base-300">Critical</td>
+						<td class="bg-base-300">{criticalcountry}</td>
+						<td class="bg-base-300 font-bold">11</td>
+						<td class="bg-base-300">Deaths Per 1 Million</td>
+						<td class="bg-base-300">{deathsPerOneMillioncountry}</td
+						>
 					</tr>
 					<tr>
-						<th class="bg-base-300">3</th>
-						<td class="bg-base-300">Total Deaths</td>
-						<td class="bg-base-300">34749</td>
-						<td class="bg-base-300 font-bold">6</td>
-						<td class="bg-base-300">Deaths Today</td>
-						<td class="bg-base-300">{deathstoday}</td>
+						<th class="bg-base-300">6</th>
+						<td class="bg-base-300">Tests</td>
+						<td class="bg-base-300">{testscountry}</td>
+						<td class="bg-base-300 font-bold">12</td>
+						<td class="bg-base-300">Tests Per 1 Million</td>
+						<td class="bg-base-300">{testsPerOneMillioncountry}</td>
 					</tr>
 				</tbody>
 			</table>
